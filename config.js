140fb77371674c3870cf77cee28fa8ec6276d266
Ishiyabotz <wathsalaudeshika36@gmail.com>
2024-10-24 17:29:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",
+SESSION_ID: process.env.SESSION_ID || "yaYHHY6b#7U48u73jAstgBbf7XdiP6NKGs4ews9x7T2R6JiPanyg",
+MONGODB: process.env.MONGODB || "mongodb+srv://isithaanusara01:202213isi@cluster0.mcee3.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://unitedcamps.in/Images/file_4270.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
 AUTO_BIO:"true",
